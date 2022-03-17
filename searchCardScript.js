9f201f33c0fdf13a6879d944774ee240a8598f7c
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2022-03-17 00:09:12 +0000

    Initial commit

---
+++ searchCardScript.js
@@ -0,0 +1,23 @@
+
+
+
+//This function will create a text file that contains the custom banlist the user generated!
+function generateBanlist() {
+    var select = document.getElementById("limitation");
+    var value = select.options[select.selectedIndex].value;
+
+
+    //value.toString();
+
+    var textBanlist = String(value);
+    console.log(textBanlist);
+    var blob = new Blob(["0000" + " " + textBanlist + "\n" + "0000" + " " + textBanlist],
+        { type: "text/plain;charset=utf-8" }
+    );
+    saveAs(blob, "banlist.txt");
+}
+
+/*This is a jQuery Trial function!
+$("limitation").click(function () {
+    console.log("jQuery Clicked!!");
+});*/
