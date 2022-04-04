713bad208c0f3e3d34702811bbfbca587f4dd104
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2022-04-04 04:34:18 +0100

    questionButtonNeedToBeFixed
    Need to ask questions and then the next button should move to the next one and so on.

--- BanlistCreator.js
+++ BanlistCreator.js
@@ -1,3 +1,4 @@
+let count;
 //Workflow: Click 'CreateBanlist' You'll get a banlist view.
 function CreateNewBanlist() {
     let banListPreview = document.getElementById('banListPreview');
@@ -12,7 +13,45 @@ function ImportBanlist() {
 
 //AddItemBtn: When you click it a questionaire will appear 
 function addItem() {
-    console.log('Questionaire Should Start!');
+
+    let nextBtn = document.getElementById('next');
+    //This should enable the qustions (You may show them as divs aswell)
+    //@1 Show first question: Whitelist or not!
+    let itemQuestions = [];
+    itemQuestions[0] = document.getElementById('whiteListBox');
+    itemQuestions[1] = document.getElementById('limitation-selection');
+
+
+    count = 0;
+
+    if (count == 0) {
+        itemQuestions[0].style.display = "block";
+
+    }
+
+    else if (count == 1) {
+        console.log("Count is 1");
+    }
+
+    /*  nextBtn.addEventListener("click", () => {
+          let whiteListCheck = document.querySelector('.form-check-input').checked;
+          if (whiteListCheck) {
+              alert("Whitelist Checked!");
+              //If it's true then add whitelist to the text array (for the final blob)
+          }
+          itemQuestions[1].style.display = "block";
+  
+      });
+  
+      nextBtn.addEventListener("click", () => {
+  
+          alert("Question 2 started!1!");
+  
+  
+      });
+  
+      //itemQuestions[1] = "2";
+      console.log(itemQuestions[0] + '\n' + itemQuestions[1]);*/
 }
 
 function removeItem() {
@@ -25,4 +64,9 @@ function randomizeItems() {
 
 function clearItems() {
     console.log("Items were reset");
+}
+
+function Next() {
+    count++;
+    console.log(count);
 }
