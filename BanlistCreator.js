0e2bddddc727d6ef34bfa043382d7d888bbcab7f
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2022-04-04 03:36:57 +0100

    buttonsWorking
    Buttons were linked to their script, but still not functional!

--- BanlistCreator.js
+++ BanlistCreator.js
@@ -1,8 +1,8 @@
 //Workflow: Click 'CreateBanlist' You'll get a banlist view.
 function CreateNewBanlist() {
-    let divToAnimate = document.getElementById('animate');
-    divToAnimate.style.display = 'block';
-    console.log("Banlist View Should Appear At The Bottom!");
+    let banListPreview = document.getElementById('banListPreview');
+    banListPreview.style.display = 'flex';
+
 }
 
 //ًWorkflow: Click 'ImportBanlist' You'll import an already list with their data appearing on the banist view.
@@ -10,9 +10,19 @@ function ImportBanlist() {
     console.log('Success! You imported a banlist.');
 }
 
-/*@TrialCode@
-function animate() {
-    let divToAnimate = document.getElementsByClassName('animate');
-    divToAnimate.style.display = block;
+//AddItemBtn: When you click it a questionaire will appear 
+function addItem() {
+    console.log('Questionaire Should Start!');
+}
 
-}*/
+function removeItem() {
+    console.log("Item Removed!");
+}
+
+function randomizeItems() {
+    console.log("Items were randomized!");
+}
+
+function clearItems() {
+    console.log("Items were reset");
+}
